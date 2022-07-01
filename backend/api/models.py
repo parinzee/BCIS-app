b102669f5cba2db291979aaeef5f744440d4ecbf
Parinthapat Pengpun <parinzee@protonmail.com>
2022-07-01 09:42:34 +0700

    Removed custom user model in favor of proxy models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,87 +1,40 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-
-# User models
-
-
-class UserManager(BaseUserManager):
-    def create_user(
-        self, email, first_name, last_name, user_type, team_color=None, password=None
-    ):
-
-        if not email:
-            raise ValueError("Users must have an email address")
-
-        user = self.model(
-            email=email,
-            first_name=first_name,
-            last_name=last_name,
-            user_type=user_type,
-            team_color=team_color,
-            password=password,
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password=None):
-        """
-        Creates and saves a superuser with the given email, date of
-        birth and password.
-        """
-        user = self.create_user(
-            email,
-            "Admin",
-            "Admin",
-            user_type="S",
-            team_color=None,
-            password=password,
-        )
-
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
 
-class User(AbstractBaseUser):
-    USER_TYPES = (
-        ("S", "Staff"),
-        ("P", "Parents"),
-        ("KS", "Kindergarten Student"),
-        ("ES", "Elementary Student"),
-        ("HS", "Highschool Student"),
-    )
-    TEAM_COLORS = (
-        ("R", "Red Team"),
-        ("G", "Green Team"),
-        ("B", "Blue Team"),
-        ("Y", "Yellow Team"),
-    )
+DEPARTMENT = (
+    ("K", "Kindergarten"),
+    ("E", "Elementary"),
+    ("H", "Highschool"),
+)
 
-    username = None
-    email = models.EmailField("email address", max_length=100, unique=True)
-    first_name = models.CharField("first name", max_length=30)
-    last_name = models.CharField("last name", max_length=30)
-    user_type = models.CharField("student type", choices=USER_TYPES, max_length=2)
+TEAM_COLORS = (
+    ("R", "Red Team"),
+    ("G", "Green Team"),
+    ("B", "Blue Team"),
+    ("Y", "Yellow Team"),
+)
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    department = models.CharField("department", choices=DEPARTMENT, max_length=2)
     team_color = models.CharField(
         "team color", choices=TEAM_COLORS, max_length=1, blank=True, null=True
     )
 
-    objects = UserManager()
+    push_ids = ArrayField(models.CharField(max_length=60), size=50)
 
-    is_admin = models.BooleanField("is admin", default=False)
 
-    USERNAME_FIELD = "email"
+class Staff(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    team_color = models.CharField(
+        "team color", choices=TEAM_COLORS, max_length=1, blank=True, null=True
+    )
+    push_ids = ArrayField(models.CharField(max_length=60), size=50)
 
-    def __str__(self):
-        return self.email
 
-    def has_perm(self, perm, obj=None):
-        return True
-
-    def has_module_perms(self, app_label):
-        return True
-
-    def is_staff(self):
-        return self.is_admin
+class Parent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    push_ids = ArrayField(models.CharField(max_length=60), size=50)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
@@ -122,5 +122,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-AUTH_USER_MODEL = "api.User"
