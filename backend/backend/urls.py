2b8f7ff60c558d92ac1fcf7d063f5dd72eb5a088
Parinthapat Pengpun <parinzee@protonmail.com>
2022-07-06 12:44:03 +0700

    Moved api app into project directory

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,10 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from backend.api import views
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
+
 
 urlpatterns = [
+    path("", include(router.urls)),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
 ]
