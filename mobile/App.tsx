39832cf58e618c35bad5475b98846bf1a63396ad
Parinthapat Pengpun <parinzee@protonmail.com>
2022-07-09 09:03:51 +0700

    Finished basic tab bar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import Navigation from "./navigation";
+import { Provider as PaperProvider } from "react-native-paper";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+        <PaperProvider>
+          <Navigation />
+          <StatusBar />
+        </PaperProvider>
       </SafeAreaProvider>
     );
   }
